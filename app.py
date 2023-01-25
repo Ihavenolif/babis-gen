ea67ecb6339276a0ce9d8f1da9432c6239ca7282
Ihavenolife <matuswall@gmail.com>
2023-01-25 21:01:14 +0100

    pre-release beta or something

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import random
 from flask import Flask, render_template, request
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -18,13 +19,23 @@ def index():
 
     response = openai.Completion.create(
         model = "text-davinci-003",
-        prompt = "Respond to this question like Andrej Babiš. Respond in Czech. Make a lot of excuses.\n\n" + query,
-        temperature=0.75,
+        prompt = "Respond to this question like Andrej Babiš in the latest presidential debate. Include recent news about presidential elections in the Czech republic. Respond in Czech. Make a lot of excuses. Be very mean and rude. Sound angry and annoyed. Don't talk directly about the topic.\n\n" + query,
+        temperature=0.9,
         max_tokens=500
     )
     response_text = response.get("choices")[0].get("text")
 
-    return response_text
+    final_text = ""
+
+    response_split = response_text.split(" ")
+
+    for x in response_split:
+        if ("te" in x or "de" in x or "ne" in x) and random.random() < 0.2:
+            final_text += x.replace("te", "tě").replace("de", "dě").replace("ne", "ně") + " "
+        else:
+            final_text += x + " "
+
+    return final_text
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
